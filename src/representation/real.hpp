eecfa732956f4a875e67912b5c911846de2b3b57
Ben Jones <bjones@MacBook-Pro.local>
2020-02-20 18:37:24 +0000

    add namespacing to real type

--- src/representation/real.hpp
+++ src/representation/real.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
-// Default precision for double. Might want to
-// compiler-switch on this (@jp)
-using real = long double;
+namespace arrow {
+	// Default precision for double. Might want to
+	// compiler-switch on this (@jp)
+	using real = long double;
+}
